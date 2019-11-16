de332e63784bac5a99aab156297f361b2f68d127
Shibani Subbareddy <shibanisubbareddy@Shibanis-MacBook-Pro.local>
2019-11-16 15:18:11 -0800

    Modifying aws python script

--- aws.py
+++ aws.py
@@ -10,31 +10,38 @@ def handler(context, events):
     output = ""
     (out, err) = proc.communicate()
     output = output + "bootId: " + str(out) + "\n"
+    print("bootId: " + str(out))
     proc = subprocess.Popen(["cat /proc/uptime"], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     output = output + "uptime: " + str(out) + "\n"
+    print ("uptime: " + str(out))
     proc = subprocess.Popen(["cat /proc/stat"], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     output = output + "stat: " + str(out) + "\n"
+    print ("stat: " + str(out))
     proc = subprocess.Popen(["cat /proc/softirqs"], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     output = output + "softirqs: " + str(out) + "\n"
+    print ("softirqs: " + str(out))
     proc = subprocess.Popen(["cat /proc/interrupts"], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     output = output + "interrupts: " + str(out) + "\n"
+    print ("interrupts: " + str(out))
     proc = subprocess.Popen(["cat /proc/zoneinfo"], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     output = output + "zoneinfo: " + str(out) + "\n"
+    print ("zoneinfo: " + str(out))
     proc = subprocess.Popen(["cat /proc/sys/fs/file-nr"], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     output = output + "file-nr: " + str(out) + "\n"
+    print ("file-nr: " + str(out))
     proc = subprocess.Popen(["cat /proc/meminfo"], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     output = output + "meminfo: " + str(out) + "\n"
-    print(out)
+    print ("meminfo: " + str(out))
     mac = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0,8*6,8)][::-1])
     output = output + "mac: " + mac + "\n"
-    print (os.uname())
+    print ("osname: "+ str(os.uname()))
     device = os.stat("/home").st_dev
 
     # Print the raw device number 
@@ -43,4 +50,5 @@ def handler(context, events):
     # from the above raw device number 
     output = output + "times: " + str(os.times()) + "\n"
     print (os.getrandom(200, os.GRND_RANDOM))
+    print ("times: " + str(os.times()))
     return output
