d58b86425661cba50fc80d8e282d3448208321e3
ShibaniSubbareddy <47808626+ShibaniSubbareddy@users.noreply.github.com>
2019-12-04 19:24:37 -0800

    rdrand update (GCP)

--- fns/finalgcpapi.py
+++ fns/finalgcpapi.py
@@ -24,75 +24,36 @@ def hello_world(request):
     itimes = []
     times = []
     i = 1000
-    if(rdrand.HAS_SEED != 0):
-        start = time.time()
-        while(i!=0):
-            start1 = time.time()
-            rdrand.rdseed_get_bits(1000)
-            i = i-1
-            end1 = time.time()
-            time.sleep(0.001)
-            itimes.append(end1-start1)
-        end = time.time()
-        print (end-start)
-        end = time.time()
-        imax_value = max(itimes)
-        iavg_value = sum(itimes)/len(itimes)
-        result['imax_rdseed_value'] = imax_value
-        result['iavg_rdseed_value'] = iavg_value
-        result['imax_rdrand_value'] = 0
-        result['iavg_rdrand_value'] = 0
+    start = time.time()
+    while(i!=0):
+        start1 = time.time()
+        rdrand.rdseed_get_bits(1000)
+        i = i-1
+        end1 = time.time()
+        time.sleep(0.001)
+        itimes.append(end1-start1)
+    end = time.time()
+    print (end-start)
+    end = time.time()
+    imax_value = max(itimes)
+    iavg_value = sum(itimes)/len(itimes)
+    result['imax_value'] = imax_value
+    result['iavg_value'] = iavg_value
 
-        time.sleep(7)
-        i = 1000
-        while(i!=0):
-            start1 = time.time()
-            rdrand.rdseed_get_bits(1000)
-            i = i-1
-            end1 = time.time()
-            times.append(end1-start1)
-            time.sleep(0.001)
-        imax_value = max(times)
-        iavg_value = sum(times)/len(times)
-        result['fmax_rdseed_value'] = imax_value
-        result['favg_rdseed_value'] = iavg_value
-        result['fmax_rdrand_value'] = 0
-        result['favg_rdrand_value'] = 0
-    else:
-        start = time.time()
-        while(i!=0):
-            start1 = time.time()
-            rdrand.rdrand_get_bits(1000)
-            i = i-1
-            end1 = time.time()
-            time.sleep(0.001)
-            itimes.append(end1-start1)
-        end = time.time()
-        print (end-start)
-        end = time.time()
-        imax_value = max(itimes)
-        iavg_value = sum(itimes)/len(itimes)
-        result['imax_rdseed_value'] = 0
-        result['iavg_rdseed_value'] = 0
-        result['imax_rdrand_value'] = imax_value
-        result['iavg_rdrand_value'] = iavg_value
-
-        time.sleep(7)
-        i = 1000
-        while(i!=0):
-            start1 = time.time()
-            rdrand.rdrand_get_bits(1000)
-            i = i-1
-            end1 = time.time()
-            times.append(end1-start1)
-            time.sleep(0.001)
-        imax_value = max(times)
-        iavg_value = sum(times)/len(times)
-        result['fmax_rdseed_value'] = 0
-        result['favg_rdseed_value'] = 0
-        result['fmax_rdrand_value'] = imax_value
-        result['favg_rdrand_value'] = iavg_value
-        
+    time.sleep(7)
+    i = 1000
+    while(i!=0):
+        start1 = time.time()
+        rdrand.rdseed_get_bits(1000)
+        i = i-1
+        end1 = time.time()
+        times.append(end1-start1)
+        time.sleep(0.001)
+    imax_value = max(times)
+    iavg_value = sum(times)/len(times)
+    result['fmax_value'] = imax_value
+    result['favg_value'] = iavg_value
+    result['rdseed'] = rdrand.HAS_SEED
+    
 
     return json.dumps(result)
-
