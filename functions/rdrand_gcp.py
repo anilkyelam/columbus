191bd160bc21a7703453e2f8ca6cd44d6d743c3c
Shibani Subbareddy <shibanisubbareddy@Shibanis-MacBook-Pro.local>
2019-12-05 15:47:19 -0800

    Modifying aws and gcp rdrand files

--- functions/rdrand_gcp.py
+++ functions/rdrand_gcp.py
@@ -4,19 +4,37 @@ import random
 import uuid
 import json
 import time
-import datetime
+import psutil
+import platform
+from datetime import datetime
 import rdrand
 
 def hello_world(request):
     result = {}
     result['start_time'] = int(round(time.time() * 1000))
-
-
+    up_times = open('/proc/uptime', mode='r').read().split()
+    result['UP Time 1'] = up_times[0]
+    boot_time_timestamp = psutil.boot_time()
+    bt = datetime.fromtimestamp(boot_time_timestamp)
+    result['boottime'] = str(bt)
+    # CPU frequencies
+    cpufreq = psutil.cpu_freq()
+    result["Current Frequency"]= cpufreq.current
+    # CPU usage
+    
+    # get all network interfaces (virtual and physical)
+    if_addrs = psutil.net_if_addrs()
+    for interface_name, interface_addresses in if_addrs.items():
+        for address in interface_addresses:
+            result["Interface:"]= interface_name
+            if str(address.family) == 'AddressFamily.AF_INET':
+                result["IP Address"] =address.address
 
     mac = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0,8*6,8)][::-1])
     result["MAC Addr"] = mac
 
     result['IP'] = os.uname()[1]
+
     # result["osname"] = str(os.uname())
 
     # Extract the device minor number 
@@ -25,6 +43,7 @@ def hello_world(request):
     times = []
     i = 5000
     if(rdrand.HAS_SEED != 0):
+        result['rdseed']=1
         start = time.time()
         while(i!=0):
             start1 = time.time()
@@ -60,6 +79,7 @@ def hello_world(request):
         result['favg_rdrand_value'] = 0
     else:
         i = 5000
+        result['rdseed']=0
         start = time.time()
         while(i!=0):
             start1 = time.time()
