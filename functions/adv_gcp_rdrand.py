af6ee10fb5b6613888cfa6d0b89d562e510854bd
keerthana96 <keerthana96@gmail.com>
2019-12-08 16:42:10 -0800

    Added gcp adversary results

--- functions/adv_gcp_rdrand.py
+++ functions/adv_gcp_rdrand.py
@@ -10,13 +10,18 @@ from datetime import datetime
 import rdrand
 
 def hello_world(request):
+    start = time.time()
     result = {}
+    result["In"] = "Both"
     result['Start Time'] = int(round(time.time() * 1000))
+    time.sleep(6)
+    
     up_times = open('/proc/uptime', mode='r').read().split()
     result['UP Time'] = up_times[0]
     boot_time_timestamp = psutil.boot_time()
     bt = datetime.fromtimestamp(boot_time_timestamp)
     result['Boot Time'] = str(bt)
+    
     # CPU frequencies
     cpufreq = psutil.cpu_freq()
     result["Frequency"]= cpufreq.current
@@ -31,25 +36,24 @@ def hello_world(request):
     mac = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0,8*6,8)][::-1])
     result["MAC Address"] = mac
 
-    i = 150000
-    num_bits = 10000
+    i = 100000
+    num_bits = 100000
     if(rdrand.HAS_SEED != 0):
         result['Rdseed Enabled']=1
-        start = time.time()
+        start1 = time.time()
         while(i!=0):
             rdrand.rdseed_get_bits(num_bits)
             i = i-1
         end = time.time()
-        print (end-start)
 
     else:
         result['Rdseed Enabled']=0
-        start = time.time()
+        start1 = time.time()
         while(i!=0):
             rdrand.rdrand_get_bits(num_bits)
             i = i-1
         end = time.time()
-        print (end-start)
-
-    result["Exec Time"] = end - start
+    
+    result["Total Time"] = end - start
+    result["Exec Time"] = end - start1
     return json.dumps(result)
