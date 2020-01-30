e357365b3335a3ce40dcb9a62954a72b1b04c572
Shibani Subbareddy <shibanisubbareddy@shibanis-mbp.home>
2020-01-30 11:34:04 -0800

    Default URL

--- api/run_adv.py
+++ api/run_adv.py
@@ -40,7 +40,7 @@ def worker():
 
 def main():
 
-    defaulturl = "https://a0bn4o2c71.execute-api.ap-east-1.amazonaws.com/latest"    # ap-east-1
+    defaulturl = "https://6fdhepb68i.execute-api.me-south-1.amazonaws.com/latest"    # ap-east-1
     #defaulturl = "https://v1pe8p505h.execute-api.us-west-2.amazonaws.com/latest"   # us-west-2
     #defaulturl = "https://h6r05ug8ga.execute-api.me-south-1.amazonaws.com/latest"  # me-south-2
     #defaulturl = "https://8rq4dl2w8f.execute-api.us-west-1.amazonaws.com/latest"   # us-west-1
@@ -48,7 +48,7 @@ def main():
     parser = argparse.ArgumentParser("Makes concurrent requests to lambda URLs")
     parser.add_argument('-c', '--count', action='store', type=int, help='number of requests to make (max:{0})'.format(MAX_CONCURRENT), default=5)
     parser.add_argument('-o', '--out', action='store', help='path to results file', default="results.csv")
-    parser.add_argument('-u', '--url', action='store', help='url to call', required=True) #default=defaulturl)
+    parser.add_argument('-u', '--url', action='store', help='url to call', default=defaulturl)
     parser.add_argument('-a', '--attack', action='store_true', help='whether to run thrashers', default=False)
     args = parser.parse_args()
 
