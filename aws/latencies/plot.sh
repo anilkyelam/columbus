7b72c7e2ccd4705179b434bdfd611069a0c1d75f
Anil kumar <anilkyelam@github.com>
2020-07-07 00:52:19 -0700

    plot aesthetic edits 2

--- aws/latencies/plot.sh
+++ aws/latencies/plot.sh
@@ -1,4 +1,4 @@
-python plot.py  -z bar -l "AWS" \
+python plot.py  -z bar -l "AWS"  --fontsize=25 \
     -xc "Offset" -xl "Offset (Bytes)" \
     -yc "Latencies" -yl "Kilo CPU Cycles" --ylim 8 --ymul 0.001 \
     -d latencies -o membus_aws.pdf
--- azure/plot.sh
+++ azure/plot.sh
@@ -1,12 +1,12 @@
 
-python plot.py  -z bar -l "Azure" \
+python plot.py  -z bar -l "Azure" --fontsize=25 \
     -xc "Offset" -xl "Offset (Bytes)" \
     -yc "Latencies" -yl "" --ylim 8 --ymul 0.001 \
     -d results/latencies -o results/membus_azure.pdf
 display results/membus_azure.pdf &
 
 # GCP Placeholder
-python plot.py  -z bar -l "GCP" \
+python plot.py  -z bar -l "GCP" --fontsize=25 \
     -xc "Offset" -xl "Offset (Bytes)" \
     -yc "Latencies" -yl "" --ylim 8 --ymul 0.001 \
     -d results/temp_gcp -o results/membus_gcp.pdf
