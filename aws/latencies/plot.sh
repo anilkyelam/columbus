172d05a2708672678e4b2523741d908df6a9b8b5
Anil kumar <anilkyelam@github.com>
2020-07-07 01:20:38 -0700

    plot aesthetic changes 3

--- aws/latencies/plot.sh
+++ aws/latencies/plot.sh
@@ -1,5 +1,5 @@
-python plot.py  -z bar -l "AWS"  --fontsize=25 \
+python plot.py  -z bar -l "AWS"  --fontsize=25 --lloc=topleft \
     -xc "Offset" -xl "Offset (Bytes)" \
-    -yc "Latencies" -yl "Kilo CPU Cycles" --ylim 8 --ymul 0.001 \
+    -yc "Latencies" -yl "CPU Cycles" --ylim 8000 --ylog \
     -d latencies -o membus_aws.pdf
 display membus_aws.pdf
